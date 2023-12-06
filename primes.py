1478c5531e1f9d6c26f0635dbed1a5cb72247ba4
Emily Lazenby <124580748+elaz345@users.noreply.github.com>
2023-12-06 17:49:49 +0000

    Done

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+
+    while len(list) < number_of_primes:
+        is_prime = all(num % prime != 0 for prime in list)
+
+        if is_prime:
+            list.append(num)
+
+        num += 1
+
     return list
